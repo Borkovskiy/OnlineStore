b12226289e50d16a9f7961d143913f377c8f8d94
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-08-04 00:45:50 +0300

    v0.9.1

--- src/main/java/project/store/onlinestore/OnlineStoreApplication.java
+++ src/main/java/project/store/onlinestore/OnlineStoreApplication.java
@@ -13,16 +13,7 @@ public class OnlineStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineStoreApplication.class, args);
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("http://localhost:8080");
-            }
-        };
-
-    }
+   
 
 
 }
