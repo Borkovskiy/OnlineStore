151598b8905f7f14b2d1fced8245ee3919d3853f
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-08-10 17:34:54 +0300

    slider v2

--- src/main/java/project/store/onlinestore/model/Slider.java
+++ src/main/java/project/store/onlinestore/model/Slider.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 
 public class Slider {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Lob
     private byte [] image;
