9487f158ce75ce73a146f04409211e852ea49c7e
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-08-23 15:03:54 +0300

    cors

--- src/main/java/project/store/onlinestore/OnlineStoreApplication.java
+++ src/main/java/project/store/onlinestore/OnlineStoreApplication.java
@@ -2,6 +2,7 @@ package project.store.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class OnlineStoreApplication {
