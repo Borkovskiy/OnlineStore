84c4fce141c93571b16a5a9bc3ece390137365d0
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-08-27 14:23:37 +0300

    4 test bd

--- src/main/java/project/store/onlinestore/model/Slider.java
+++ src/main/java/project/store/onlinestore/model/Slider.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 
 public class Slider {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @Lob
     private byte [] image;
