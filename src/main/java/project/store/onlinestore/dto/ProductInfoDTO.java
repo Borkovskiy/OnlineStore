baf636c188ad0bcf755ac19f85dc7bd2cb48699a
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-08-27 18:58:01 +0300

    full test bd

--- src/main/java/project/store/onlinestore/dto/ProductInfoDTO.java
+++ src/main/java/project/store/onlinestore/dto/ProductInfoDTO.java
@@ -40,7 +40,11 @@ public class ProductInfoDTO {
 
     public static List<String> toBase64(List<ProductImage> productImages) {
         List<String> base64Image = new ArrayList<>();
-        productImages.forEach((p) -> base64Image.add(Base64.getEncoder().encodeToString(p.getImage())));
+        //productImages.forEach((p) -> base64Image.add(Base64.getEncoder().encodeToString(p.getImage())));
+        for (int i= 0;i<productImages.size();i++){
+
+            base64Image.add(Base64.getEncoder().encodeToString(productImages.get(i).getImage()));
+        }
         return base64Image;
     }
 }
