6c3fc935343d9519a3c737c05e7a0893fe6763e8
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-08-29 18:14:51 +0300

    new version of emailDTO

---
+++ src/main/java/project/store/onlinestore/dto/EmailDTO.java
@@ -0,0 +1,14 @@
+package project.store.onlinestore.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EmailDTO {
+    private String email;
+
+    public EmailDTO(String email) {
+        this.email = email;
+    }
+}
