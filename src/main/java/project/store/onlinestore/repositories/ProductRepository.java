94176fbc9de7010306d2416d69d501e2999c7e8a
Borkovskiy <80268043+Borkovskiy@users.noreply.github.com>
2021-09-01 13:50:15 +0300

    first refactor

--- src/main/java/project/store/onlinestore/repositories/ProductRepository.java
+++ src/main/java/project/store/onlinestore/repositories/ProductRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findProductByProductStatus(Pageable pageable, ProductStatus productStatus);
+    List<Product> findProductByProductStatusAndAndProductImagesOrderByIdIdAsc(Pageable pageable, ProductStatus productStatus);
 
 }
